ad9cecaba3800a914b1062057b748ca4c65d5da2
syarif-mh <241697312+syarif-mh@users.noreply.github.com>
2025-12-30 16:25:22 +0700

    Create a new function named playRound
    This function will takes the human and computer
    player choices as arguments, plays a single round,
    increments the round winner's score and logs
    a winner announcement.

--- javascript.js
+++ javascript.js
@@ -5,9 +5,31 @@ function getComputerChoice() {
     return comChoice = choices[Math.floor(Math.random() * max)];
 }
 
+
 function getHumanChoice() {
     return userChoice = prompt("Enter you choice! (rock, paper, or scissors)").toLowerCase();
 }
 
+
 let humanScore = 0;
 let computerScore = 0;
+
+
+function playRound(humanChoice, comChoice) {
+    humanChoice = humanChoice.replace(humanChoice[0], str => str.toUpperCase())
+    console.log('You choose:',humanChoice)
+    comChoice = comChoice.replace(comChoice[0], str => str.toUpperCase())
+    console.log('Computer choose:',comChoice)
+
+    if ((humanChoice == 'Rock' && comChoice == 'Scissors') || (humanChoice == 'Paper' && comChoice == 'Rock') || (humanChoice == 'Scissors' && comChoice == 'Paper')) {
+        console.log(`You won! ${humanChoice} beats ${comChoice}`)
+        return humanScore = humanScore + 1;
+    } else if ((comChoice == 'Rock' && humanChoice == 'Scissors') || (comChoice == 'Paper' && humanChoice == 'Rock') || (comChoice == 'Scissors' && humanChoice == 'Paper')) {
+        console.log(`You lose! ${comChoice} beats ${humanChoice}`)
+        return computerScore = computerScore + 1
+    } else if (humanChoice == comChoice) {
+        return console.log('Tie!')
+    } else {
+        return alert('invalid Choice! Choice must be rock, paper, or scissors');
+    }
+}
