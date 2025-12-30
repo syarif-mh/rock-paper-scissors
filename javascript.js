6513fa063c2b0fd6def2e6e8c2bf36513057682f
syarif-mh <241697312+syarif-mh@users.noreply.github.com>
2025-12-30 16:17:48 +0700

    Create a getHumanChoice function
    This function will prompt user to choose between
    rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -4,3 +4,7 @@ function getComputerChoice() {
     let max = choices.length;
     return comChoice = choices[Math.floor(Math.random() * max)];
 }
+
+function getHumanChoice() {
+    return userChoice = prompt("Enter you choice! (rock, paper, or scissors)").toLowerCase();
+}
